e46cb762e293fd56ffac2fdb80abbe065cc59ee3
Matthew Hasselfield <mhasself@users.noreply.github.com>
2018-02-03 16:19:36 -0500

    Add GB/s column.

--- test_np_filt.py
+++ test_np_filt.py
@@ -6,22 +6,26 @@ import npfilt
 # On feynman, 8 threads, this settles down to 0.33 seconds per buffer.
 n_chan = 1024*6
 n_samp = 30000
+n_byte = 4
+n_payload = n_chan * n_samp * n_byte
 
-print 'Creating %s samples of gaussian white noise...' % n_samp
+print('Creating %s samples of gaussian white noise...' % n_samp)
 dat0 = np.zeros((n_chan, n_samp), 'int32')
 dat0[0] = np.random.normal(scale=100, size=(n_samp))
 
-print 'Broadcasting to %i channels.' % n_chan
+print('Broadcasting to %i channels.' % n_chan)
 dat0[1:] = dat0[0:1,:]
 dat1 = np.zeros((n_chan, n_samp), dat0.dtype)
 
-print 'Filtering.'
+print('Filtering.')
 import time
 t0 = time.time()
 n_iter = 0
+n_data = 0
 while True:
     npfilt.apply_filter(dat0, dat1, 0)
     n_iter += 1
+    n_data += n_payload
     dt = time.time() - t0
-    print n_iter, dt, dt/n_iter
+    print('%4i %8.3f %8.4f %9.4f' % (n_iter, dt, dt/n_iter, n_data/ dt / 1e9))
 
